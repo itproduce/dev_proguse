6ecc7bdfb89a951c5eaf45a15f79f70d3db0aeae
itproduce <61737744+itproduce@users.noreply.github.com>
2020-03-03 15:29:53 +0300

    КОМ

--- PHP.php
+++ PHP.php
@@ -29,3 +29,19 @@ $arPrice = CCatalogProduct::GetOptimalPrice($productID, $quantity, $USER->GetUse
 		echo "</pre>";
 
 ?>
+<?
+//	вывод цены отдельно 
+$arPrice = CCatalogProduct::GetOptimalPrice($productID, $quantity, $USER->GetUserGroupArray(), $renewal);
+		if (!$arPrice || count($arPrice) <= 0)
+		{
+		    if ($nearestQuantity = CCatalogProduct::GetNearestQuantityPrice($productID, $quantity, $USER->GetUserGroupArray()))
+		    {
+		        $quantity = $nearestQuantity;
+		        $arPrice = CCatalogProduct::GetOptimalPrice($productID, $quantity, $USER->GetUserGroupArray(), $renewal);
+		    }
+		}
+		echo "<pre>";
+		print_r($arPrice);
+		echo "</pre>";
+
+?>
